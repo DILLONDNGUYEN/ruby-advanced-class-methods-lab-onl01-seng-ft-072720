990480c0f012e86b86284c0f782c97daf704838b
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 05:48:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 # .create instantiates and saves the song, and it returns the new song that was create
 # .new_by_name instantiates a song with a name property
-# .create_by_name
-    instantiates and saves a song with a name property (FAILED - 3)
+# .create_by_nameinstantiates and saves a song with a name property (FAILED - 3)
+    
   .find_by_name
     can find a song present in @@all by name (FAILED - 4)
     returns falsey when a song name is not present in @@all (FAILED - 5)
