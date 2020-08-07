f7df80ebf026c7e0623c6300695c7ad7fe22b2b0
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 05:51:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,13 @@
 # .create_by_name instantiates and saves a song with a name property 
 # .find_by_name can find a song present in @@all by name returns falsey when a song name is not present in @@all
 # .find_or_create_by_name invokes .find_by_name and .create_by_name instead of repeating codereturns the existing Song object (doesn't create a new one) when provided the title of an existing Song creates a new Song object with the provided title if one doesn't already exist
-#    
+# .alphabetical   
     
     
     
    
     
-  .alphabetical
+  
     returns all the song instances in alphabetical order by song name (FAILED -9)
   .new_from_filename
     initializes a song and artist_name based on the filename format (FAILED - 10)
